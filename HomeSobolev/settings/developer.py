2f204cc7b9e42588cae5bc22386d7085bb001345
Groofidze <groofi@list.ru>
2022-05-10 15:40:04 +0300

    Update v3.5

--- HomeSobolev/settings/developer.py
+++ HomeSobolev/settings/developer.py
@@ -1,3 +1,5 @@
+from HomeSobolev.settings import BASE_DIR
+
 # Режим разработки
 DEBUG = True
 
@@ -6,5 +8,9 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Статические файлы
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 
